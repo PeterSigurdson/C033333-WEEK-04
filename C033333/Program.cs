c905749f82e24ab956dcd7cfa2007d1325e89cd6
peter sigurdson <peter@petersigurdson.net>
2019-01-30 11:18:47 -0500

    I added the CountrySide class and Launch method to initialize the program!

--- C033333/Program.cs
+++ C033333/Program.cs
@@ -18,7 +18,29 @@ namespace C033333
         // Node is an ADT
         // what KIND of DATA do we need in a NODE?
         public Village nextVillage;
+        public Village previousVillage;
         public String VillageName;
         public bool isAstrildeHere = false;
+    }
+    class Countryside
+    {
+        Village Maple = new Village();
+        Village Toronto = new Village();
+        Village Ajax = new Village();
+        Village First;
+        Village Last;
+ 
+        public void Launch()
+        {
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousVillage = Maple; 
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null; 
+            Ajax.nextVillage = Toronto;
+        }
     }
 }
