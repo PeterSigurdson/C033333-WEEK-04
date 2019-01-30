57b8c033fa7d5694812b9e349dd6a24991f5aedb
peter sigurdson <peter@petersigurdson.net>
2019-01-30 10:21:00 -0500

    I ADDED CODE FOR THE VILLAGE NODE

--- C033333/Program.cs
+++ C033333/Program.cs
@@ -12,4 +12,13 @@ namespace C033333
         {
         }
     }
+
+    class Village
+    {
+        // Node is an ADT
+        // what KIND of DATA do we need in a NODE?
+        public Village nextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+    }
 }
